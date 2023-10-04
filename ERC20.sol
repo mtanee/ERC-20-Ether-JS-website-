3a79f3c65d188b0c9a078b72fb8821b6143acd80
Taneer <114029687+mtanee@users.noreply.github.com>
2023-10-04 19:55:21 +0500

    Create ERC20.sol

---
+++ ERC20.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./IERC20.sol";
+
+contract Taneer is IERC20 {
+    string public name = "Taneer";
+    string public symbol = "TEST";
+    uint8 public decimals = 18;
+    uint256 private _totalSupply;
+    address private _owner;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor(uint256 initialSupply) {
+        _totalSupply = initialSupply * 10**uint256(decimals);
+        _owner = msg.sender;
+        _balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0),msg.sender,_totalSupply);
+        
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(recipient != address(0), "ERC20: Transfer to the zero address");
+        require(_balances[msg.sender] >= amount, "ERC20: Insufficient balance");
+
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(sender != address(0), "ERC20: Transfer from the zero address");
+        require(recipient != address(0), "ERC20: Transfer to the zero address");
+        require(_balances[sender] >= amount, "ERC20: Insufficient balance");
+        require(_allowances[sender][msg.sender] >= amount, "ERC20: Allowance exceeded");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        _allowances[sender][msg.sender] -= amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+}
